125579b237e3ba936d2813165d0f8980768526ad
tasneem <tasneem@192.168.186.128>
2021-04-19 12:30:20 -0700

    first commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by tasneem on 4/19/21.
+//  Copyright © 2021 tasneem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
