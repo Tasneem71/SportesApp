961edd046da41008f1c1837c07abc5cb6512dbc9
Donia Ashraf <doniaashraf@Donias-Mac.local>
2021-04-21 01:40:12 +0200

    create collection view cell

---
+++ SportsApp/Controllers/AllSports/AllSportsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AllSportsViewController.swift
+//  SportsApp
+//
+//  Created by Donia Ashraf on 4/21/21.
+//  Copyright © 2021 tasneem. All rights reserved.
+//
+
+import UIKit
+
+class AllSportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SportsApp/Controllers/AllSports/SportCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SportCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Donia Ashraf on 4/21/21.
+//  Copyright © 2021 tasneem. All rights reserved.
+//
+
+import UIKit
+
+class SportCollectionViewCell: UICollectionViewCell {
+    
+}
